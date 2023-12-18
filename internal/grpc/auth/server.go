9b841bda47315ac418100ba1a78dc5b7dcd65fe0
xamust <xamust@gmail.com>
2023-12-18 09:40:10 +0300

    add login validation

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -16,10 +16,21 @@ func Register(gRPC *grpc.Server) {
 }
 
 func (s *serverAPI) Login(ctx context.Context, lr *authServerv1.LoginRequest) (*authServerv1.LoginResponse, error) {
-	if err := xvalidator.NewXValidator().ValidateVar(lr.Email, "email"); err != nil {
-		return nil, ValidationError("email", err)
+	// todo: think about calmdown...
+	if err := loginValidation(lr); err != nil {
+		return nil, err
 	}
 	return &authServerv1.LoginResponse{Token: "token for " + lr.Email}, nil
+}
+
+func loginValidation(req *authServerv1.LoginRequest) error {
+	if err := xvalidator.NewXValidator().ValidateVar(req.Email, "email"); err != nil {
+		return ValidationError("email", err)
+	}
+	if err := xvalidator.NewXValidator().ValidateVar(req.Password, "required"); err != nil {
+		return ValidationError("password", err)
+	}
+	return nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, rr *authServerv1.RegisterRequest) (*authServerv1.RegisterResponse, error) {
